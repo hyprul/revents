06b3bcb3732192a495f54f24c28b027b724fe53e
Sean Huang <seanh@ritani.com>
2019-12-28 22:13:44 -0500

    track

--- revents/src/App.js
+++ revents/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React
+          React is Hot! No Reload! Testing Commit
         </a>
       </header>
     </div>
